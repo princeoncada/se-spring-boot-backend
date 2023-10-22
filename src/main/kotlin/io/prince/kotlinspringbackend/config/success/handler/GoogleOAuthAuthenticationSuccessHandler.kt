027b945f956d36aba3032f6f367d5eb6aa2a894c
Prince Oncada <prince.oncada@gmail.com>
2023-10-22 18:07:38 +0800

    cookie fixing

--- src/main/kotlin/io/prince/kotlinspringbackend/config/success/handler/GoogleOAuthAuthenticationSuccessHandler.kt
+++ src/main/kotlin/io/prince/kotlinspringbackend/config/success/handler/GoogleOAuthAuthenticationSuccessHandler.kt
@@ -49,7 +49,7 @@ class GoogleOAuthAuthenticationSuccessHandler(
             val cookie = Cookie("jwtToken", token).apply {
                 maxAge = 3600
                 path = "/"
-                domain = frontEndUrl
+                domain = "kotling-spring-backend-production.up.railway.app"
             }
             response.addCookie(cookie)
             response.sendRedirect("${frontEndUrl}/authenticate")
