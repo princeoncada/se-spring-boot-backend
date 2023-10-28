392513681cf4653f086777d490d72861064b9322
Prince Oncada <prince.oncada@gmail.com>
2023-10-29 07:34:45 +0800

    init commit

---
+++ project-ocp/src/main/kotlin/com/mmcm/projectocp/ProjectOcpApplication.kt
@@ -0,0 +1,11 @@
+package com.mmcm.projectocp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectOcpApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProjectOcpApplication>(*args)
+}
---
+++ project-ocp/src/test/kotlin/com/mmcm/projectocp/ProjectOcpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mmcm.projectocp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectOcpApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
